d6a46bdf92a980914b2d773fdb3677bc4894386e
Sarthak Kohli <Sarthak.kohli.0.7@gmail.com>
2018-03-23 16:26:28 +0530

    Create graph.py

---
+++ graph.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import style
+import time
+
+style.use("ggplot")
+
+
+fig = plt.figure()
+axis = fig.add_subplot(1,1,1)
+
+
+def animate(i):
+
+    data = open("twitter-out.txt", "r").read()
+    lines = data.split('\n')
+
+    xar = []
+    yar = []
+
+    x = 0
+    y = 0
+
+    for l in lines:
+        x += 1
+        if "pos" in l:
+            y += 1
+        elif "neg" in l:
+            y -= 1
+
+        xar.append(x)
+        yar.append(y)
+
+    axis.clear()
+    axis.plot(xar,yar)
+
+ani = animation.FuncAnimation(fig, animate, interval=1000)
+plt.show()
+
